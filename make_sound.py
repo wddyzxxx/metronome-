2a097f8bdc284d969f5c414e8b3ed0228ad77578
Yuan_zhen <90907990+wddyzxxx@users.noreply.github.com>
2022-03-16 19:18:13 +0800

    Update make_sound.py

--- make_sound.py
+++ make_sound.py
@@ -7,22 +7,27 @@ class Beat:
         self.bpm = bpm
         self.num_of_beat = num_of_beat
 
-    def make_sound(self):
+
+    def secondary(self):
+        interval = int((60000 / self.bpm) * 4 / self.num_of_beat)
+        for _ in range(1, self.num_of_beat):
+            winsound.Beep(1000, interval)
+
+    def primary(self):
+        interval = int((60000 / self.bpm) * 4 / self.num_of_beat)
+        winsound.Beep(1500, interval)
+
+    def metronome(self):
         while True:
-            interval = int((60000 / self.bpm) * 4 / self.num_of_beat)
-            # start = time.time()
-            winsound.Beep(4000, interval)
-            for _ in range(1, self.num_of_beat):
-                winsound.Beep(1000, interval)
-                # time.sleep(1)
-                # end = time.time()
-                # print(end - start)
+            Beat.primary(self)
+            Beat.secondary(self)
+
 
 def initiate():
     num_of_beat=int(input("请输入几分音符为一拍："))
     bpm=int(input("请输入想要的节拍速度："))
     a=Beat(num_of_beat, bpm)
-    a.make_sound()
+    a.metronome()
 
 initiate()
 
